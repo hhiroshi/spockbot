bd2859d34c76f77523be378826173154f4573d37
hhiroshi <hiromoto.hiroshi@gmail.com>
2015-09-25 21:03:32 -0500

    Adding Slack configuration to realtime url

--- app.rb
+++ app.rb
@@ -16,22 +16,26 @@ end
 
 get '/realtime' do
 
-client = Slack::RealTime::Client.new
+	Slack.configure do |config|
+		  config.token = "xoxb-11404662370-wWxE6Kd5mEwFNzFDngexHsAK"
+		end
 
-client.on :hello do
-  puts "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
-end
+	client = Slack::RealTime::Client.new
 
-client.on :message do |data|
-  case data['text']
-  when 'bot hi' then
-    client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
-  when /^bot/ then
-    client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, what?"
-  end
-end
+	client.on :hello do
+	  puts "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
+	end
 
-client.start!
+	client.on :message do |data|
+	  case data['text']
+	  when 'bot hi' then
+	    client.message channel: data['channel'], text: "Hi <@#{data['user']}>!"
+	  when /^bot/ then
+	    client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, what?"
+	  end
+	end
+
+	client.start!
 
 end
 
