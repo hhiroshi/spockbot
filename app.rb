e8d9fe8ca076e02456f236954d5ef574011420a6
hhiroshi <hiromoto.hiroshi@gmail.com>
2015-09-27 22:01:28 -0500

    Adding dashboard

--- app.rb
+++ app.rb
@@ -41,7 +41,7 @@ get '/' do
 			else
 				p "ERROR SCORE #{data['user']} - #{Time.now}"
 			end
-			client.message channel: data['channel'], text: "Gracias <@#{data['user']}>!"
+			client.message channel: data['channel'], text: "Gracias <@#{data['user']}>! He recolectado tu happiness index de hoy."
 		else
 			p "MENSAJE NO MAPEADO: #{data['text']}"
 		end
@@ -57,13 +57,56 @@ get '/get-happiness' do
 
 	client = Slack::Web::Client.new
 
-	#p client.users_list
+	p client.users_list
 
 	#client.im_list['ims'].each do |im|
-	#	client.chat_postMessage(channel: im['id'], text: '¿Cómo estás hoy? :D', as_user: true)
+	#	client.chat_postMessage(channel: im['id'], text: '¿Cómo estás hoy? (Del 1 al 5)', as_user: true)
 	#end
 
-	client.chat_postMessage(channel: 'D0BBQ0Z5J', text: '¿Cómo estás hoy? :smile:', as_user: true)
+	client.chat_postMessage(channel: 'D0BBQ0Z5J', text: '¿Cómo estás hoy? (Del 1 al 5)', as_user: true)
+end
 
+get '/dashboard' do
+
+	hhiroshi_score = 0
+	hhiroshi_counter = 0
+
+	gustavo_score = 0
+	gustavo_counter = 0
+
+	snahider_score = 0
+	snahider_counter = 0
+
+	jessy_score = 0
+	jessy_counter = 0
+
+	#U02GBG3PJ gus
+	#U02GAHTFT snahider
+	#U096S6KMM jessy
+
+	Score.all.each do |score_item|
+
+		if score_item.user_id == "U02GAHMCZ"
+			hhiroshi_score += score_item.value.to_i
+			hhiroshi_counter += 1
+		elsif score_item.user_id == "U02GBG3PJ"
+			gustavo_score += score_item.value.to_i
+			gustavo_counter += 1
+		elsif score_item.user_id == "U02GAHTFT"
+			snahider_score += score_item.value.to_i
+			snahider_counter += 1
+		elsif score_item.user_id == "U096S6KMM"
+			jessy_score += score_item.value.to_i
+			jessy_counter += 1	
+		end
+
+	end
+
+	@hhiroshi_happiness_index =  hhiroshi_score/hhiroshi_counter unless hhiroshi_counter==0
+	@gustavo_happiness_index =  gustavo_score/gustavo_counter unless gustavo_counter==0
+	@snahider_happiness_index =  snahider_score/snahider_counter unless snahider_counter==0
+	@jessy_happiness_index =  jessy_score/jessy_counter unless jessy_counter==0
+
+	erb :dashboard
 
 end
